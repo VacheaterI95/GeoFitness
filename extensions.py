e2f5ea512b16c46b9252f983ffc9d95db39dd24b
VacheaterI95 <vache.meliqishvili@gmail.com>
2024-06-30 23:40:33 +0400

    extensions.py

--- extensions.py
+++ extensions.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+
 
 
 
